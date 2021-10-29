ee7454bf7af6e08fb4ec26dfa0604e9b22752b84
Sagi Perekhodnik <sagipere2004@gmail.com>
2021-10-29 12:01:24 -0700

    Implemented status check

--- midnight_rider.py
+++ midnight_rider.py
@@ -1,24 +1,74 @@
 # Midnight Rider
 
+import sys
+import textwrap
+import time
 import midnight_rider_text
 
-
-# A text based game of intrigue and illusion
+# A text-based game of intrigue and illusion
 
 
 class Game:
-    """Represents the game engine
-    
+    """Represent our game engine
+    Attribute:
+        done: describes if the game is
+            finished or not - bool
+        distance_travelled: describe the distance that weve travelled so far this game,
+            in units
+        amount_of_tofu: how much tofu we have left in our inventory
+            agents_distance: describes the distance between the player and the agents
     """
+    def __init__(self):
+        self.done = False
+        self.distance_travelled = 0
+        self.amount_tofu = 3
+        self.agents_distance = -20
+
 
     def introduction(self) -> None:
         """Print the introduction text"""
-        print(midnight_rider_text.INTRODUCTION)
+        self.typewriter_effect(midnight_rider_text.INTRODUCTION)
 
+    def typewriter_effect(self, text: str) -> None:
+        """Print out to console with a typewriter effect."""
+        for char in textwrap.dedent(text):
+            time.sleep(0.05)
+            sys.stdout.write(char)
+            sys.stdout.flush()
+
+    def show_choices(self) -> None:
+        """Show the user their choices"""
+        time.sleep(1)
+        print(midnight_rider_text.CHOICES)
+        time.sleep(1)
+
+    def get_choice(self) -> None:
+        """Gets the user's choice and changes
+        the environment"""
+        # Get the user's response
+        user_choice = input().strip(",.?!").lower()
+
+        # Based on their choice, change the attributes
+        # of the class
+        if user_choice == "e":
+            print("---Status Check---")
+            print(f"Distance Travelled: {self.distance_travelled} units")
+            print(f"Tofu Pieces Left: {self.amount_tofu}")
+            print(f"Agent's Distance:{abs(self.agents_distance)} units behind")
+            print("------")
+            time.sleep(2)
+        elif user_choice == "q":
+            self.done = True
 
 def main() -> None:
-    game = Game()  # starting a new game
-    game.introduction()
+    game = Game()   # starting a new game
+    # game.introduction()
+
+    while not game.done:
+        game.show_choices()
+        game.get_choice()
+        # TODO: Check win/lose conditions
+
 
 
 if __name__ == "__main__":
