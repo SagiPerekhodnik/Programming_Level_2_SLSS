6b3f33466cc6334a54dbd2a63ef4ec26237f8c74
Sagi Perekhodnik <sagipere2004@gmail.com>
2021-11-09 08:47:56 -0800

    Added pygame drawing

---
+++ pygame_drawing.py
@@ -0,0 +1,25 @@
+# Pygame_Drawing
+# Author: Sagi
+# 9 November 2021
+
+# Get introduced to Pygame and draw objects on screen
+
+import pygame
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT= 600
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_WIDTH)
+
+def main() -> None:
+    """Driver of the Python script"""
+
+if__name__ == "main":
+    main()
