3ecf06e14826813af4268f3acfb7729d82186fab
Sagi Perekhodnik <sagipere2004@gmail.com>
2021-11-23 12:13:22 -0800

    Created blocks

---
+++ collecting_blocks.py
@@ -0,0 +1,119 @@
+# Collecting Blocks Example
+# Author: Ubial
+
+
+import random
+import pygame
+
+pygame.init()
+
+WHITE =     (255, 255, 255)
+BLACK =     (  0,   0,   0)
+RED   =     (255,   0,   0)
+GREEN =     (  0, 255,   0)
+BLUE  =     (  0,   0, 255)
+ETON_BLUE = (135, 187, 162)
+RAD_RED =   (255,  56, 100)
+BLK_CHOCOLATE = (25, 17, 2)
+
+BGCOLOUR =  WHITE
+
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE  = "Collecing Blocks"
+
+
+class Block(pygame.sprite.Sprite):
+    """Describes a block object
+    A subclass of pygame.sprite.Sprite
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self, colour: tuple, width: int, height: int) -> None:
+        """
+        Arguments:
+        :param colour: 3-tuple (r, g, b)
+        :param width: width in pixels
+        :param height: height in pixels
+        """
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.Surface([width, height])
+        self.image.fill(colour)
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
+
+def main() -> None:
+    """Driver of the Python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+    num_blocks = 100
+
+
+    # Create groups to hold sprites
+    all_sprites = pygame.sprite.Group()
+    block_sprites = pygame.sprite.Group()
+
+    # Create all the block sprites and add to block_sprites
+    for i in range(num_blocks):
+        # Create a block (set its parameters)
+        block = Block(BLACK, 18, 12, )
+
+
+
+        # Set a random location for the block inside screen
+        block.rect.x = random.randrange(SCREEN_WIDTH)
+        block.rect.y = random.randrange(SCREEN_HEIGHT)
+        # Add the block_sprites Group
+        # Add the block to all_sprites Group
+        block_sprites.add(block)
+        all_sprites.add(block)
+
+    # Create the Player block
+    player = Block(ETON_BLUE, 20, 15)
+    # Add the Player to all_sprites group
+    all_sprites.add(player)
+
+    pygame.mouse.set_visible(False)
+
+
+    # ----------- MAIN LOOP
+    while not done:
+        # ----------- EVENT LISTENER
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ----------- CHANGE ENVIRONMENT
+        # Process player movement based on mouse pos
+
+        mouse_pos = pygame.mouse.get_pos()
+        player.rect = mouse_pos
+
+        # ----------- DRAW THE ENVIRONMENT
+        screen.fill(BGCOLOUR)      # fill with bgcolor
+
+        # Draw all sprites
+        all_sprites.draw(screen)
+
+        # Update the screen
+        pygame.display.flip()
+
+        # ----------- CLOCK TICK
+        clock.tick(75)
+
+
+if __name__ == "__main__":
+    main()
